eb0906092186621287cc9c8073910c6ec47d25c2
Andry Pebrianto <andrypeb227@gmail.com>
2021-12-17 03:25:27 +0700

    Create utils and types

---
+++ src/redux/actions/types.js
@@ -0,0 +1,6 @@
+// Auth
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAILED = "LOGIN_FAIL";
+export const LOGOUT = "LOGOUT";
+export const AUTH_SUCCESS = "AUTH_SUCCESS";
+export const AUTH_FAIL = "AUTH_FAIL";
