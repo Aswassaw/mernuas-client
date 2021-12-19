388bb699e26098af5032911030651857958013a0
Andry Pebrianto <andrypeb227@gmail.com>
2021-12-20 03:29:56 +0700

    Access .env not require dotenv package, so uninstall it

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,7 @@
-import dotenv from "dotenv";
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
-
-dotenv.config();
 
 const initialState = {};
 const middleWare = [thunk];
